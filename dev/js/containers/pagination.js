d23fe1c792e0f7bab2f674cd9e90ed7e65ae1ec2
pradeepnedun <pnedunchezhiyan@sapient.com>
2018-05-24 14:51:34 +0530

    pagination chevron enable/disable logic added

--- dev/js/containers/pagination.js
+++ dev/js/containers/pagination.js
@@ -19,82 +19,98 @@ class Pagination extends Component {
           this.props.setPaginationCurrentPage(this.props.id , 'ONLOAD');
     }
     updateCurrentPage(e) {
-        let obj = {};
+        let currentObj = {};
         this.props.currentPage.forEach(element => {
             if(element.id === this.props.id) {
-                obj = element;
+                currentObj = element;
             }
         });
         if(e.target.dataset.type === 'PREV') {
-            if(obj.page >= 1) {
+            if(currentObj.page >= 1) {
                 this.props.setPaginationCurrentPage(this.props.id , e.target.dataset.type, this.getMaxPageCount());  
             } else {
                 this.props.setPaginationCurrentPage(this.props.id , 'PAGENUMBER');
-                this.props.reqObj.pageNumber = obj.ajaxCount;
+                this.props.reqObj.pageNumber = currentObj.ajaxCount;
                 this.props.fetchListData(this.props.reqObj, this.props.id);
             }
         }
         if(e.target.dataset.type === 'NEXT') {
-            if(obj.page <= this.getMaxPageCount()) {
+            if(currentObj.page <= this.getMaxPageCount()) {
                 this.props.setPaginationCurrentPage(this.props.id , e.target.dataset.type, this.getMaxPageCount());  
             } else {
                 this.props.setPaginationCurrentPage(this.props.id , 'PAGENUMBER');
-                this.props.reqObj.pageNumber = obj.ajaxCount;
+                this.props.reqObj.pageNumber = currentObj.ajaxCount;
                 this.props.fetchListData(this.props.reqObj, this.props.id);
             }
         }
         if(e.target.dataset.type === 'FIRST') {
-            if(obj.page <= this.getMaxPageCount()) {
+            if(currentObj.page <= this.getMaxPageCount()) {
                 this.props.setPaginationCurrentPage(this.props.id , e.target.dataset.type, this.getMaxPageCount());  
             } else {
                 this.props.setPaginationCurrentPage(this.props.id , 'PAGENUMBER');
-                this.props.reqObj.pageNumber = obj.ajaxCount;
+                this.props.reqObj.pageNumber = currentObj.ajaxCount;
                 this.props.fetchListData(this.props.reqObj, this.props.id);
             }
         }
         if(e.target.dataset.type === 'LAST') {
-            if(Math.ceil(this.getMaxPageCount()/ (60 / this.props.recordsPerPage)) === Math.ceil(obj.page / (60 / this.props.recordsPerPage))) {
+            if(Math.ceil(this.getMaxPageCount()/ (60 / this.props.recordsPerPage)) === Math.ceil(currentObj.page / (60 / this.props.recordsPerPage))) {
                 this.props.setPaginationCurrentPage(this.props.id , e.target.dataset.type, this.getMaxPageCount());
             } else {
                 this.props.setPaginationCurrentPage(this.props.id , 'PAGENUMBER');
-                this.props.reqObj.pageNumber = obj.ajaxCount;
+                this.props.reqObj.pageNumber = currentObj.ajaxCount;
                 this.props.fetchListData(this.props.reqObj, this.props.id); 
             }
         }
     }
+
     renderList() {
-        let resultsflag = false, disableAllChevrons = false, obj = {};
+        let resultsflag = false, disableAllChevrons = false, 
+            disableFirstChevron = false, disablePrevChevron = false, disableNextChevron = false, disableLastChevron = false, 
+            currentObj = {};
         if(this.props.query.length > 0 && this.props.currentPage.length > 0 && this.props.id !== null) {
-            if(this.props.query[0].totalresults > 0) {
-                resultsflag = true;
-                if(this.props.query[0].totalresults < this.props.recordsPerPage) {
-                    disableAllChevrons = true;
-                }
-            }
             if(this.props.currentPage.length > 0) {
                 this.props.currentPage.forEach(element => {
                     if(element.id === this.props.id) {
-                        obj = element;
+                        currentObj = element;
                     }
                 });
             }
+            //3 conditons to remove disable / enable chevron - start
+            if(this.props.query[0].totalresults > 0) {
+                resultsflag = true;
+                if(this.props.query[0].totalresults < this.props.recordsPerPage) {
+                    disablePrevChevron = true;
+                    disableFirstChevron = true;
+                    disableNextChevron = true;
+                    disableLastChevron = true;
+                }
+            }
+            if(currentObj.page === this.getMaxPageCount()) {
+                disableNextChevron = true;
+                disableLastChevron = true;
+            }
+            if(currentObj.page === 1) {
+                disablePrevChevron = true;
+                disableFirstChevron = true;
+            }
+            //3 conditons to remove disable / enable chevron - end
             return <div>
                 <div className="pagination top">
-                    <a className={disableAllChevrons ? 'first disabled' : 'first'} onClick={this.updateCurrentPage.bind(this)}>
+                    <a className={(disableFirstChevron) ? 'first disabled' : 'first'} onClick={this.updateCurrentPage.bind(this)}>
                         <i data-type="FIRST" className="fa fa-step-backward"></i> 
                     </a>
-                    <a className={disableAllChevrons ? 'prev disabled' : 'prev'} onClick={this.updateCurrentPage.bind(this)}>
+                    <a className={(disablePrevChevron) ? 'prev disabled' : 'prev'} onClick={this.updateCurrentPage.bind(this)}>
                         <i data-type="PREV" className="fa fa-angle-left"></i> 
                     </a>
-                    <a className={disableAllChevrons ? 'next disabled' : 'next'} onClick={this.updateCurrentPage.bind(this)}>
+                    <a className={(disableNextChevron) ? 'next disabled' : 'next'} onClick={this.updateCurrentPage.bind(this)}>
                         <i data-type="NEXT" className="fa fa-angle-right"></i>
                     </a>
-                    <a className={disableAllChevrons ? 'last disabled' : 'last'} onClick={this.updateCurrentPage.bind(this)}>
+                    <a className={(disableLastChevron) ? 'last disabled' : 'last'} onClick={this.updateCurrentPage.bind(this)}>
                         <i data-type="LAST" className="fa fa-step-forward"></i>
                     </a>
                     <p className="page-count">
                     <strong>sida 
-                        <span className="cur-page">{obj.page}</span>
+                        <span className="cur-page">{currentObj.page}</span>
                     </strong> av 
                     <span className="max-page">{this.getMaxPageCount()}</span>
                     </p>
